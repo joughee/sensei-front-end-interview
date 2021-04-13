b94d29ebe2bd8a54d0077ab6ec3ec87762420331
joughee <65375852+joughee@users.noreply.github.com>
2021-04-12 17:09:50 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <h1>{{ userName }}</h1>
+    <h2>{{ currentDate }}</h2>
+    <div class="task-columns">
+      <div v-for="column in columns" :key="column.title">
+        <status-column v-bind:column="column"></status-column>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import StatusColumn from './components/StatusColumn.vue'
+
+export default {
+  data() {
+    return {
+      userName: "Joe",
+      currentDate: new Date(),
+      columns: [
+        {
+          title: "Backlog",
+          tasks: ["Backlog 1","Backlog 2"]
+        },        
+        {
+          title: "In Progress",
+          tasks: []
+        },
+        {
+          title: "QA",
+          tasks: []
+        },
+        {
+          title: "Done",
+          tasks: []
+        }
+      ]
+    }
+  },
+  components: {
+    StatusColumn
+
+  }
+}
+</script>
+
+<style scoped>
+  h2 {
+    border-bottom: #000 1px;
+  }
+  .task-columns {
+    display: flex;
+  }
+</style>
---
+++ src/components/StatusColumn.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="task-col">
+        <h2>{{ title }}</h2>
+        <div>{{ tasks.length }} Tasks</div>
+        <ul class="task-list">
+            <li class="task-item" v-for="task in tasks" :key="task">{{ task }}</li>
+        </ul>
+        <button @click="addTask">+ Add Task</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: { column: Object },
+    data() {
+        return {
+            title: this.column.title,
+            tasks: this.column.tasks
+        }
+    },
+    methods: {
+        addTask() {
+            this.tasks.push(window.prompt('Task Description'))
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .task-col{
+        margin: 10px;
+        padding: 10px;
+    }
+    .task-list {
+        list-style: none;
+        background: #000;
+        color: #FFF;
+        height: 500px;
+
+    }
+    .task-item {
+        color: #000;
+        background-color: #FFF;
+    }
+</style>
